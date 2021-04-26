b010c9243320c54028bf7ce07e5a9bbdd2621757
GameModeDebil <magicazarus@gmail.com>
2021-04-26 21:05:00 +0200

    scripts
    transition between locations + main gameplay

---
+++ ClickerAndroid/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int score;
+    public static int gainOnClick;
+    public static int passiveGain;
+    public static int passiveScore;
+      void Start()
+    {
+        
+        score = PlayerPrefs.GetInt("score",0); 
+        gainOnClick = PlayerPrefs.GetInt("gainOnClick",1);
+        passiveGain = PlayerPrefs.GetInt("passiveGain",1);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+
+}
---
+++ ClickerAndroid/Assets/Scripts/MainScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScript : MonoBehaviour
+{
+    public Text score, passiveScoreValue;
+    public GameObject Street, Room, Home, Bums;
+    
+    public void Incriment()
+    {
+        
+        GameManager.score += GameManager.gainOnClick;
+        
+    }
+    IEnumerator ScorePerSec()
+    {
+        GameManager.score += GameManager.passiveGain;
+        yield return new WaitForSeconds (1f);
+        StartCoroutine (ScorePerSec());
+
+    }
+    public void goingToLocation (int num)
+    {
+        if (num == 1)
+        {
+        Street.SetActive(true);
+        Room.SetActive(false);
+        Home.SetActive(false);
+        Bums.SetActive(false);
+        }
+        if (num == 2)
+        {
+        Street.SetActive(false);
+        Room.SetActive(false);
+        Home.SetActive(true);
+        Bums.SetActive(false);
+        }
+        if (num == 3)
+        {
+        Street.SetActive(false);
+        Room.SetActive(true);
+        Home.SetActive(false);
+        Bums.SetActive(false);
+        }
+       
+    }
+    void Start()
+    {
+        StartCoroutine (ScorePerSec()); 
+        Street.SetActive(true);
+        Room.SetActive(false);    
+        Home.SetActive(false);
+        Bums.SetActive(false);
+       
+    }
+
+    
+    void Update()
+    {
+        PlayerPrefs.SetInt("score", GameManager.score);
+        score.text = "Бутылачки: " + GameManager.score;
+        passiveScoreValue.text = "Бомжи приносят:" + GameManager.passiveGain;
+    }
+}
